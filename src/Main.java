e2aab2e68ed1ce0de7263876aa64c4b24aee1b74
Alvin <alvinl70@nycstudents.net>
2022-10-11 17:39:53 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Nice to see you!\n");
+        System.out.println("Goodbye!");
+
+        //And
+        System.out.println("Nice to see you!");
+        System.out.println("Goodbye!");
+
     }
 }
